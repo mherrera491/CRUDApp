09fc1276e79b3d4398cb2fb4a70e3fa89d8d9523
mherrera491 <mattherrera1@gmail.com>
2023-12-01 12:40:49 -0800

    fixed register/create user issue

--- controllers/users.js
+++ controllers/users.js
@@ -13,6 +13,10 @@ users.get("/new", (req, res) => {
 users.post("/", (req, res) => {
     req.body.password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10))
     User.create(req.body, (err, createdUser) => {
+        if (err) {
+            console.log(err);
+        }
+        req.session.currentUser = createdUser;
         console.log('User is created', createdUser)
         res.redirect("/")
     })
